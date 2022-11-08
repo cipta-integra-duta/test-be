ecee57b88a9876e90d22e6962a54a94d4a9cc7d2
arfather <arfather26@gmail.com>
2022-11-08 14:23:10 +0700

    readme

--- chapter 01/routes/api.php
+++ chapter 01/routes/api.php
@@ -7,113 +7,124 @@ use Illuminate\Support\Facades\Route;
 // panggil endpoint ini dengan apps front end
 // point 0 : handling Cross-Origin Resource Sharing (CORS)
 Route::get('/user', function (Request $request) {
-    $users = User::orderBy('created_at', 'DESC')->paginate(10);
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => $users
-      ], 200);
+  $users = User::orderBy('created_at', 'DESC')->paginate(10);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => $users
+  ], 200);
 });
 
-// point 1 : handling Cross-Origin Resource Sharing (CORS)
 // make migration
 // add field gender, location, photo. make field nullable
 // change field password to nullable
 // make seeder users: name: arfin, male, arfin.mustofa@integra.co.id, [random_password], jakarta
 
-// point 2 : create new user
+// point 2 : login user
+Route::delete('/user/login', function (Request $request) {
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => [
+      "token" => "ABC"
+    ]
+  ], 200);
+});
+
+// point 3 : create new user
 Route::post('/user/create', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "create"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "create"
+  ], 200);
 });
 
-// point 3 : update new user
+// point 4 : update new user
 Route::post('/user/update', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "update"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "update"
+  ], 200);
 });
 
-// point 4 : delete new user
+// point 5 : delete new user
 Route::delete('/user/delete', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "delete"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "delete"
+  ], 200);
 });
 
-// point 5 : import new user / bulk insert
+// point 6 : import new user / bulk insert
 Route::post('/user/bulk-insert', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "bulk"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "bulk"
+  ], 200);
 });
 
-// point 5 : delete multiple user / bulk delete
+// point 7 : delete multiple user / bulk delete
 Route::post('/user/bulk-delete', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "delete"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "delete"
+  ], 200);
 });
 
-// point 6 : export user to excel, field: name, email, gender, location
+// point 8 : export user to excel, field: name, email, gender, location
 Route::get('/user/export-excel', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "excel"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "excel"
+  ], 200);
 });
 
-// point 7 : export user to pdf, field: name, email, gender, location
+// point 9 : export user to pdf, field: name, email, gender, location
 Route::get('/user/export-pdf', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "pdf"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "pdf"
+  ], 200);
 });
 
-// point 8 : upload a photo user, validation with max photo is 4MB
+// point 10 : upload a photo user, validation with max photo is 4MB
 Route::get('/user/upload', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "upload"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "upload"
+  ], 200);
 });
 
-// point 8 : send an email to user
+// point 11 : send an email to user
 // subject: TEST BE
 // body: hello [name_user]
 Route::get('/user/send-email/{id}', function (Request $request) {
-    return response()->json([
-        'code' => 200,
-        'status' => true,
-        'message' => "Success",
-        'data' => "upload"
-      ], 200);
+  return response()->json([
+    'code' => 200,
+    'status' => true,
+    'message' => "Success",
+    'data' => "upload"
+  ], 200);
 });
 
-// point 9 : make a kernel command / cronjob to send an email to user
+// point 12 : make a kernel command / cronjob to send an email to user
 // subject: Happy Birthday [name_user]
 // body: semoga sehat selalu...
---
+++ readme.md
@@ -0,0 +1,26 @@
+## Backend Test Coding
+# Where You Code
+Make a new branch with your name and then you can use a directory APP to submit your coding.
+see the GUIDE in api router.
+
+# Chapter Completion
+* CORS              -> handle cors on hit on another service (FE)
+* Migration         -> Migrate on Laravel
+* Login             -> create a login user to get token (use Middleware on next point)
+* CRUD              -> manage data user
+* BULK manage data  -> manage data user with bulk insert / delete
+* Document View     -> make a excel and pdf document
+* Upload            -> manage upload file
+* Email             -> send email to user
+* Kernel            -> make a kernel for cronjob
+
+# Dependencies:  
+* PHP 7.4 or higher
+* Composer
+* Laravel
+* ReactJs -> you can use app in test fe
+* Node 16 or higher
+* NPM 8 or higher
+* Database Mysql
+
+This project for test coding on PT. Cipta Integra Duta, Start Editing to see magic happen!
